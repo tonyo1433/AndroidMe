4b36e2af44c253aa4ddd22447d56080c870fa594
Cris Anthony La Rosa <tonrex.larosa@gmail.com>
2017-09-08 18:41:19 +0800

    Midterm Exam

--- app/src/main/java/com/hyperdev/androidme/activities/BodyPartFragment.java
+++ app/src/main/java/com/hyperdev/androidme/activities/BodyPartFragment.java
@@ -4,5 +4,74 @@ package com.hyperdev.androidme.activities;
  * Created by TONYO on 9/8/2017.
  */
 
-public class BodyPartFragment {
+
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.hyperdev.androidme.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+
+public class BodyPartFragment extends Fragment {
+
+    private List<Integer> partnum;
+    private int position;
+    private static final String list_part = "parts_list";
+    private static final String num_part = "parts_number";
+    public BodyPartFragment(){}
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater,ViewGroup container,
+                             Bundle savedInstanceState) {
+        if(savedInstanceState != null){
+            partnum = savedInstanceState.getIntegerArrayList(list_part);
+            position = savedInstanceState.getInt(num_part);
+        }
+        View rootView = inflater.inflate(R.layout.body_part_fragment, container, false);
+
+        final ImageView imageView = (ImageView) rootView.findViewById(R.id.Fragment);
+        if(partnum !=null){
+            imageView.setImageResource(partnum.get(position));
+
+            imageView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    if(position < partnum.size()-1){
+                        position++;
+                    }else {
+                        position = 0;
+                    }
+                    imageView.setImageResource(partnum.get(position));
+                }
+            });
+        }
+        return rootView;
+    }
+
+    public void setPartList(List<Integer> partList) {
+
+        this.partnum = partList;
+    }
+
+    public void setListPosition(int mListPosition) {
+
+        this.position= mListPosition;
+    }
+
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putIntegerArrayList(list_part, (ArrayList<Integer>)partnum);
+        outState.putInt(num_part,position);
+    }
 }
--- app/src/main/java/com/hyperdev/androidme/activities/MainActivity.java
+++ app/src/main/java/com/hyperdev/androidme/activities/MainActivity.java
@@ -2,14 +2,44 @@ package com.hyperdev.androidme.activities;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-
 import com.hyperdev.androidme.R;
+import com.hyperdev.androidme.data.AndroidImageAssets;
 
-public class MainActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    public class MainActivity extends AppCompatActivity{
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+
+            if(savedInstanceState == null) {
+                BodyPartFragment headPartFragment = new BodyPartFragment();
+                headPartFragment.setPartList(AndroidImageAssets.getHeads());
+                int headPosition = getIntent().getIntExtra("headPosition", 0);
+                headPartFragment.setListPosition(headPosition);
+                getSupportFragmentManager()
+                        .beginTransaction()
+                        .add(R.id.head, headPartFragment)
+                        .commit();
+
+
+                BodyPartFragment bodyPartFragment = new BodyPartFragment();
+                bodyPartFragment.setPartList(AndroidImageAssets.getBodies());
+                int bodyPosition = getIntent().getIntExtra("bodyPosition", 0);
+                bodyPartFragment.setListPosition(bodyPosition);
+                getSupportFragmentManager()
+                        .beginTransaction()
+                        .add(R.id.body, bodyPartFragment)
+                        .commit();
+
+                BodyPartFragment legPartFragment = new BodyPartFragment();
+                legPartFragment.setPartList(AndroidImageAssets.getLegs());
+                int legPosition = getIntent().getIntExtra("legsPosition", 0);
+                legPartFragment.setListPosition(legPosition);
+                getSupportFragmentManager()
+                        .beginTransaction()
+                        .add(R.id.legs, legPartFragment)
+                        .commit();
+            }
+        }
     }
-}
